6de5275b14849c6f11e1964b3f39662de19629c7
Jan-Otto Kröpke <joe@adorsys.de>
2020-02-27 15:41:35 +0100

    Re-add tar and unzip

--- images/3.1/tests/tests.sh
+++ images/3.1/tests/tests.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 
 set -euo pipefail
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'date | grep -E "CES?T"'
 docker run --rm "${DOCKER_IMAGE}:${TAG}" bash -c 'locale | grep -E LC_ALL=.+\.UTF-8'
+
+docker run --rm "${DOCKER_IMAGE}:${TAG}" tar --version
+docker run --rm "${DOCKER_IMAGE}:${TAG}" unzip -v
 
 docker run --rm "${DOCKER_IMAGE}:${TAG}" docker --version
 docker run --rm "${DOCKER_IMAGE}:${TAG}" oc version
